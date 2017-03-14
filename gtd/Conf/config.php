2c85d54d6f68fc1db1de78ddc88fd02c92d84b60
iceash <414798080@qq.com>
2017-03-14 16:43:39 +0800

    配置

--- gtd/Conf/config.php
+++ gtd/Conf/config.php
@@ -1,5 +1,15 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+    'URL_MODEL'=>1, // 如果你的环境不支持PATHINFO 请设置为3
+    'DB_TYPE'=>'mysql',
+    'DB_PORT'=>'3306',
+    // 'DB_PREFIX'=>'u_',
+    'TMPL_CACHE_ON'=>false,
+    'VAR_PAGE'=>'p',
+
+    'DB_HOST'=>'127.0.0.1',
+    'DB_NAME'=>'gtd',
+    'DB_USER'=>'root',
+    'DB_PWD'=>'',
 );
 ?>
