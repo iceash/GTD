9609b42438a945c4fde009fcef744998bb0fec02
iceash <414798080@qq.com>
2017-05-07 21:42:18 +0800

    完工

---
+++ gtd.sql
@@ -0,0 +1,83 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50616
+Source Host           : localhost:3306
+Source Database       : gtd
+
+Target Server Type    : MYSQL
+Target Server Version : 50616
+File Encoding         : 65001
+
+Date: 2017-05-07 21:40:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for material
+-- ----------------------------
+DROP TABLE IF EXISTS `material`;
+CREATE TABLE `material` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `type` int(10) unsigned DEFAULT '0' COMMENT '0为未处理的材料，1表示已生成项目，2为参考资料，3为备忘录',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of material
+-- ----------------------------
+INSERT INTO `material` VALUES ('1', '测试，项目1122', '2');
+INSERT INTO `material` VALUES ('2', '1234444', '0');
+INSERT INTO `material` VALUES ('4', '测试材料，参考资料11', '2');
+INSERT INTO `material` VALUES ('5', '一个测试材料，关于一个创业计划', '0');
+INSERT INTO `material` VALUES ('8', '一个新的材料', '1');
+INSERT INTO `material` VALUES ('10', '一个新的材料666', '1');
+
+-- ----------------------------
+-- Table structure for plan
+-- ----------------------------
+DROP TABLE IF EXISTS `plan`;
+CREATE TABLE `plan` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plan_type` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `start_time` varchar(255) DEFAULT NULL,
+  `rate` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of plan
+-- ----------------------------
+INSERT INTO `plan` VALUES ('1', 'day', '项目：一个新的材料。日程：222', '1', '2017-5-4', '20');
+INSERT INTO `plan` VALUES ('2', 'week', '项目：一个新的材料。周计划：', '1', '2017-05-01', '20');
+INSERT INTO `plan` VALUES ('3', 'month', '本月计划，测试，测试', '1', '2017-5', '20');
+INSERT INTO `plan` VALUES ('4', 'year', '2017年，我们准备。。。。', '1', '2017', '40');
+INSERT INTO `plan` VALUES ('5', 'week', '测试周计划', '1', '2017-05-15', '40');
+INSERT INTO `plan` VALUES ('6', 'year', '今年的第二个计划', '1', '2017', '40');
+INSERT INTO `plan` VALUES ('7', 'day', '5.24日准备去。。。', '1', '2017-05-24', '0');
+INSERT INTO `plan` VALUES ('8', 'day', '测试日程', '1', '2017-05-02', '20');
+
+-- ----------------------------
+-- Table structure for project
+-- ----------------------------
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `material_id` int(11) DEFAULT NULL,
+  `content` text,
+  `period` varchar(255) DEFAULT NULL COMMENT '项目时长',
+  `date1` date DEFAULT NULL COMMENT '项目开始日期',
+  `date2` date DEFAULT NULL COMMENT '项目结束日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of project
+-- ----------------------------
+INSERT INTO `project` VALUES ('1', '8', '一个新的材料', '3个月', '2017-04-25', '2017-07-25');
+INSERT INTO `project` VALUES ('2', '10', '一个新的材料555', '1个月', '2017-04-25', '2017-05-25');
--- gtd/Lib/Action/IndexAction.class.php
+++ gtd/Lib/Action/IndexAction.class.php
@@ -1,20 +1,77 @@
 <?php
-// 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
     	$active[0] = "active";
     	$this->assign("active",$active);
+        $all_material = M("material")->select();
+        foreach ($all_material as $ma) {
+            switch ($ma["type"] + 0) {
+                case 0:
+                    $basket[] = $ma;
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    $reference[] = $ma;
+                    break;
+                case 3:
+                    $memo[] = $ma;
+                    break;
+                default:
+                    break;
+            }
+        }
+        $all_project = M("project")->select();
+        $all_plan = M("plan")->select();
+        foreach ($all_plan as $pl) {
+            switch ($pl["plan_type"]) {
+                case 'day':
+                    $day_plan[] = $pl;
+                    break;
+                case 'week':
+                    $week_plan[] = $pl;
+                    break;
+                case 'month':
+                    $month_plan[] = $pl;
+                    break;
+                case 'year':
+                    $year_plan[] = $pl;
+                    break;
+                default:
+                    break;
+            }
+        }
+        $arr["basket"] = $basket;
+        $arr["reference"] = $reference;
+        $arr["memo"] = $memo;
+        $arr["project"] = $all_project;
+        $arr["day_plan"] = $day_plan;
+        $arr["week_plan"] = $week_plan;
+        $arr["month_plan"] = $month_plan;
+        $arr["year_plan"] = $year_plan;
+        $this->assign($arr);
 		$this->show();
     }
     public function calendar(){
     	$active[0] = "active";
     	$this->assign("active",$active);
+        // 日历
+        $all_day_plan = M("plan")->where("plan_type='day'")->select();
+        $this->assign("day_plans",$all_day_plan);
+        $all_week_plan = M("plan")->where("plan_type='week'")->select();
+        $this->assign("week_plans",$all_week_plan);
+        $all_month_plan = M("plan")->where("plan_type='month'")->select();
+        $this->assign("month_plans",$all_month_plan);
+        $all_year_plan = M("plan")->where("plan_type='year'")->select();
+        $this->assign("year_plans",$all_year_plan);
 		$this->show();
     }
     public function basket(){
         // 工作篮
         $active[1] = "active";
         $this->assign("active",$active);
+        $materials = M("material")->where('type=0')->order("id DESC")->select();
+        $this->assign("materials",$materials);
         $this->show();
     }
     public function add_material(){
@@ -24,80 +81,199 @@ class IndexAction extends Action {
         $this->show();
     }
     public function add_material_post(){
+        $data["content"] = $_POST["content"];
+        $back = M("material")->add($data);
         $this->redirect('Index/basket');
     }
     public function change_material(){
         // 修改材料
         $active[1] = "active";
         $this->assign("active",$active);
+        $id = $_GET["id"];
+        $redirect = $_GET["redirect"];
+        $this->assign("redirect",$redirect);
+        $material = M("material")->where("id=$id")->find();
+        // dump($material);
+        $this->assign("material",$material);
         $this->show();
+    }
+    public function change_material_post(){
+        $data["content"] = $_POST["content"];
+        $id = $_POST["id"];
+        $redirect = $_POST["redirect"];
+        $back = M("material")->where("id=$id")->save($data);
+        $this->redirect('Index/'.$redirect);
+    }
+    public function delete_material_post(){
+        $id = $_GET["id"];
+        $redirect = $_GET["redirect"];
+        $back = M("material")->where("id=$id")->delete();
+        if ($back) {
+            $this->redirect('Index/'.$redirect);
+        }
     }
     public function project(){
         // 项目列表
         $active[2] = "active";
         $this->assign("active",$active);
+        $projects = M("project")->select();
+        $this->assign("projects",$projects);
         $this->show();
     }
     public function add_project(){
         // 新建项目
         $active[2] = "active";
         $this->assign("active",$active);
+        $material_id = $_GET["id"];
+        $this->assign("material_id",$material_id);
+        $material = M("material")->where("id=$material_id")->find();
+        $this->assign("material",$material);
         $this->show();
     }
     public function add_project_post(){
+        $material_id = $_POST["material_id"];
+        $data = $_POST;
+        $back = M("project")->add($data);
+        if ($back) {
+            M("material")->where("id=$material_id")->setField("type",1);
+        }
         $this->redirect('Index/project');
     }
     public function change_project(){
         // 修改项目
         $active[2] = "active";
         $this->assign("active",$active);
+        $id = $_GET["id"];
+        $project = M("project")->where("id=$id")->find();
+        $this->assign("project",$project);
         $this->show();
+    }
+    public function change_project_post(){
+        $id = $_POST["id"];
+        $data = $_POST;
+        M("project")->where("id=$id")->save($data);
+        $this->redirect("Index/project");
     }
     public function add_memo(){
         // 材料移植备忘录
+        $id = $_GET["id"];
+        M("material")->where("id=$id")->setField("type",3);
         $this->redirect('Index/memo');
     }
     public function memo(){
         // 备忘录
         $active[3] = "active";
         $this->assign("active",$active);
+        $materials = M("material")->where('type=3')->order("id DESC")->select();
+        $this->assign("materials",$materials);
         $this->show();
     }
     public function add_reference(){
         // 材料移植备忘录
+        $id = $_GET["id"];
+        M("material")->where("id=$id")->setField("type",2);
         $this->redirect('Index/reference');
     }
     public function reference(){
         // 参考资料
         $active[4] = "active";
         $this->assign("active",$active);
+        $materials = M("material")->where('type=2')->order("id DESC")->select();
+        $this->assign("materials",$materials);
         $this->show();
     }
     public function add_day_plan(){
         // 生成日程
         $active[2] = "active";
         $this->assign("active",$active);
+        $project_id = $_GET["project_id"];
+        $this->assign("project_id",$project_id);
+        $project = M("project")->where("id=$project_id")->find();
+        $this->assign("content",$project["content"]);
         $this->show();
     }
     public function add_plan_post(){
-        $this->redirect('Index/project');
+        $data = $_POST;
+        if ($_POST["start_time"] == "") {
+            $this->error("时间缺失");
+        }
+        M("plan")->add($data);
+        $this->redirect('Index/calendar');
     }
     public function add_week_plan(){
         // 生成周计划
         $active[2] = "active";
         $this->assign("active",$active);
+        $project_id = $_GET["project_id"];
+        $this->assign("project_id",$project_id);
+        $project = M("project")->where("id=$project_id")->find();
+        $this->assign("content",$project["content"]);
         $this->show();
     }
     public function add_month_plan(){
         // 生成月计划
         $active[2] = "active";
         $this->assign("active",$active);
+        $project_id = $_GET["project_id"];
+        $this->assign("project_id",$project_id);
+        $project = M("project")->where("id=$project_id")->find();
+        $this->assign("content",$project["content"]);
         $this->show();
     }
     public function add_year_plan(){
         // 生成年计划
         $active[2] = "active";
         $this->assign("active",$active);
+        $project_id = $_GET["project_id"];
+        $this->assign("project_id",$project_id);
+        $project = M("project")->where("id=$project_id")->find();
+        $this->assign("content",$project["content"]);
         $this->show();
+    }
+    public function change_day_plan(){
+        // 修改日程
+        $active[2] = "active";
+        $this->assign("active",$active);
+        $id = $_GET["id"];
+        $plan = M("plan")->where("id=$id")->find();
+        $this->assign("plan",$plan);
+        $this->show();
+    }
+    public function change_week_plan(){
+        // 修改周计划
+        $active[2] = "active";
+        $this->assign("active",$active);
+        $id = $_GET["id"];
+        $plan = M("plan")->where("id=$id")->find();
+        $this->assign("plan",$plan);
+        $this->show();
+    }
+    public function change_month_plan(){
+        // 修改月计划
+        $active[2] = "active";
+        $this->assign("active",$active);
+        $id = $_GET["id"];
+        $plan = M("plan")->where("id=$id")->find();
+        $this->assign("plan",$plan);
+        $this->show();
+    }
+    public function change_year_plan(){
+        // 修改年计划
+        $active[2] = "active";
+        $this->assign("active",$active);
+        $id = $_GET["id"];
+        $plan = M("plan")->where("id=$id")->find();
+        $this->assign("plan",$plan);
+        $this->show();
+    }
+    public function change_plan_post(){
+        $data = $_POST;
+        if ($_POST["start_time"] == "") {
+            $this->error("时间缺失");
+        }
+        $id=$_POST["id"];
+        M("plan")->where("id=$id")->save($data);
+        $this->redirect('Index/calendar');
+
     }
 }
